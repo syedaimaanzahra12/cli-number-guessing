563eda6444a0cf64f6e4684a2048bf2a73b1e237
Syeda Imaan Zahra <zahrasyedaimaan@gmail.com>
2024-04-15 05:28:25 -0700

    created number guessing game

---
+++ index.ts
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random()*10+5);
+
+const answers = await inquirer.prompt([
+  { name: "userGuessNumber", type: "number", message: "Please guess a number between 5-10 " },
+]);
+
+if (answers.userGuessNumber === randomNumber) {
+  console.log("Congragulations! you guessed right number.");
+} else {
+  console.log("You guessed wrong number");
+}
